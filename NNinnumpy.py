99a207e734c0d471767c7ce7c79262495aa53762
zhaojiachen <zhao_jiachen@163.com>
2019-09-16 09:16:35 -0400

    Add NNinnumpy

---
+++ NNinnumpy.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+File: NNinnumpy.py
+Project: CodeofPytorch
+Author: Jiachen Zhao
+Date: 9/16/19
+Description:
+"""
+
+# -*- coding: utf-8 -*-
+import numpy as np
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# Create random input and output data using numpy
+x =
+y =
+
+# Randomly initialize weights
+w1 =
+w2 =
+
+learning_rate = 1e-6
+for t in range(500):
+    # Forward pass: compute predicted y
+    h = x.dot(w1)
+    h_relu = np.maximum(h, 0)
+    y_pred = h_relu.dot(w2)
+
+    # Compute and print loss
+    loss = np.square(y_pred - y).sum()
+    print(t, loss)
+
+    # Backprop to compute gradients of w1 and w2 with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.T.dot(grad_y_pred)
+    grad_h_relu = grad_y_pred.dot(w2.T)
+    grad_h = grad_h_relu.copy()
+    grad_h[h < 0] = 0
+    grad_w1 = x.T.dot(grad_h)
+
+    # Update weights
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
