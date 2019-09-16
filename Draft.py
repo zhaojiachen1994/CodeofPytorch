e5752c1c56e3a8ae62132759af6b77b0629442a0
zhaojiachen <zhao_jiachen@163.com>
2019-09-16 08:53:30 -0400

    Add the draft code

---
+++ Draft.py
@@ -0,0 +1,28 @@
+"""
+File: Draft.py
+Project: CodeofPytorch
+Author: Jiachen Zhao
+Date: 9/16/19
+Description:
+"""
+import numpy as np
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+
+X, y = make_classification(n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=1,n_samples=1000)
+rng = np.random.RandomState(2)
+X += 0*rng.uniform(size=X.shape)
+# X = StandardScaler().fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+svm = SVC(gamma=2, C=1)
+svm.fit(X_train, y_train)
+score = svm.score(X_test, y_test)
+print('svm accuracy:', score)
+
+
+if __name__ == "__main__":
+    print('--------')
