d09ec74eb6571c3904e5ae8a0f64d3bca3e61245
zhaojiachen <zhao_jiachen@163.com>
2019-09-16 16:58:12 -0400

    Update NNinnumpy.py

--- NNwithTensor.py
+++ NNwithTensor.py
@@ -6,10 +6,7 @@ Author: Jiachen Zhao
 Date: 9/16/19
 Description: Referred from https://pytorch.org/tutorials/beginner/pytorch_with_examples.html
 """
-
-
 import torch
-
 
 dtype = torch.float
 device = torch.device("cpu")
@@ -20,30 +17,32 @@ device = torch.device("cpu")
 N, D_in, H, D_out = 64, 1000, 100, 10
 
 # Create random input and output data
-x = #Randomly initialize the input, point the device and dtype
-y =
+x = torch.randn(N, D_in, device=device, dtype=dtype)#Randomly initialize the input, point the device and dtype
+y = torch.randn(N, D_out, device=device, dtype=dtype)
 
 # Randomly initialize weights
-w1 =
-w2 =
+w1 = torch.randn(D_in, H, device=device, dtype=dtype)
+w2 = torch.randn(H,D_out, device=device, dtype=dtype)
+
+
 
 learning_rate = 1e-6
 for t in range(500):
     # Forward pass: compute predicted y
-    h = x.mm(w1) # x.dot(w1)
-    h_relu = h.clamp(min=0) # np.maximum(0,h)
-    y_pred = h_relu.mm(w2) # h_relu.dot(w2)
+    h = x.mm(w1)# x.dot(w1), mm for matrix multiply
+    h_relu = h.clamp(min=0)# np.maximum(0,h) torch.clamp(input, min, max) 将输入input张量每个元素的夹紧到区间 [min,max]
+    y_pred = h_relu.mm(w2)# h_relu.dot(w2)
 
     # Compute and print loss
-    loss =  # np.square(y_pred - y).mean()
+    loss = (y_pred - y).pow(2).mean().item() # np.square(y_pred - y).mean()
     if t % 100 == 99:
         print(t, loss)
 
     # Backprop to compute gradients of w1 and w2 with respect to loss
     grad_y_pred = 2.0 * (y_pred - y)
-    grad_w2 = # h_relu.T.dot(grad_y_pred)
-    grad_h_relu =  # grad_y_pred.dot(w2.T)
-    grad_h =  # grad_h_relu.copy(), Real copy
+    grad_w2 = h_relu.t().mm(grad_y_pred)# h_relu.T.dot(grad_y_pred)
+    grad_h_relu = grad_y_pred.mm(w2.t()) # grad_y_pred.dot(w2.T)
+    grad_h = grad_h_relu.clone() # grad_h_relu.copy(), Real copy in pytorch: .clone()
     grad_h[h < 0] = 0
     grad_w1 = x.t().mm(grad_h)
 
