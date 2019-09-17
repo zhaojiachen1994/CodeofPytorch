9a56560c6aff47c95fa3fcd9aa24f1789a5837f6
zhaojiachen <zhao_jiachen@163.com>
2019-09-17 15:36:25 -0400

    NN with optimizer

---
+++ NNwithoptim.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+File: NNwithoptim.py
+Project: CodeofPytorch
+Author: Jiachen Zhao
+Date: 9/17/19
+Description:
+"""
+# -*- coding: utf-8 -*-
+import torch
+
+
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+
+x = torch.randn(N, D_in)
+y = torch.randn(N, D_out)
+
+
+model = torch.nn.Sequential(
+    torch.nn.Linear(D_in, H),
+    torch.nn.ReLU(),
+    torch.nn.Linear(H, D_out),
+)
+loss_fn = torch.nn.MSELoss(reduction='sum')
+
+
+learning_rate = 1e-4
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+for t in range(500):
+
+    y_pred = model(x)
+
+    loss = loss_fn(y_pred, y)
+    if t % 100 == 99:
+        print(t, loss.item())
+
+    optimizer.zero_grad()
+
+    loss.backward()
+
+    optimizer.step()
